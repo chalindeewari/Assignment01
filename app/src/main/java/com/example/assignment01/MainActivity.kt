4cb46e49030290dc1b31cb061b6e6a55ac75e0b6
Chalani <chalaniindeevari@gmail.com>
2022-03-22 13:45:49 +0530

    Assignment01

--- app/src/main/java/com/example/assignment01/MainActivity.kt
+++ app/src/main/java/com/example/assignment01/MainActivity.kt
@@ -9,6 +9,7 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONArray
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,8 +31,19 @@ class MainActivity : AppCompatActivity() {
         val queue = Volley.newRequestQueue(this)
         val reques = StringRequest(Request.Method.GET,url,Response.Listener { response ->
 
-
-            Log.e("Error",response.toString())
+            val data = response.toString()
+            val jArray = JSONArray(data)
+            for(i in 0..jArray.length()-1){
+                var jobject = jArray.getJSONObject(i)
+                var userid = jobject.getInt("userId")
+                var id = jobject.getInt("id")
+                var title = jobject.getString("title")
+                var body = jobject.getString("body")
+                Log.e("userId",userid.toString())
+                Log.e("id",id.toString())
+                Log.e("title",title.toString())
+                Log.e("body",body.toString())
+            }
         },Response.ErrorListener {  })
         queue.add(reques)
     }
